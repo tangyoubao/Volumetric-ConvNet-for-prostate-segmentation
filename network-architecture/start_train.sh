f3df5efab834414e33d621ffc3af62a2d9b2d74c
yulequan <ylqzd2011@gmail.com>
2017-07-15 16:52:35 +0800

    Update 3D-Caffe library

--- network-architecture/start_train.sh
+++ network-architecture/start_train.sh
@@ -1,8 +1,7 @@
 LOGDIR=./training_log
-CAFFE=/data/f0/gds/lqyu/deep_learning/caffe/build/tools/caffe
+CAFFE=../3D-Caffe/build/tools/caffe
 SOLVER=./solver.prototxt
 mkdir snapshot
 mkdir $LOGDIR
 
-GLOG_log_dir=$LOGDIR $CAFFE train -solver $SOLVER -gpu 2
-
+GLOG_log_dir=$LOGDIR $CAFFE train -solver $SOLVER -gpu 0
